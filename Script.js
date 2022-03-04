cfb8b9361145fa773d7ee114f5ed7e32e74ca977
Theresa Dillon <97997820+UniqueAquarius@users.noreply.github.com>
2022-03-03 21:20:23 -0500

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,31 @@
+// Const (Used for variables with a value that will NOT change)//*Value is consistent*
+let num = 20;// declared value
+console.log("num");
+
+num = 15;
+
+num = + 7;// num==7
+
+num--;
+
+num = "This is not a number";
+
+let blank;
+
+console.log("blank");//=sets the value
+// 9
+blank=null;
+
+//BONUS
+
+const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01;
+// const [quarter, dime, nickel, penny];
+
+console.log(quarter, nickle, dime , penny);
+
+let letters = "abcdefghijklmnopqrstuvwxy";
+
+console.log(letters.length);
+console.log(letters[13]);
+//console.log(letters.slice(9));
+//console.log(letters.slice(-1));
